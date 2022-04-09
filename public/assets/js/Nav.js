8fd21786011d0a0d47fa2b915475d1b78280a3b8
Skycel9 <cece.burst@gmail.com>
2022-04-09 23:19:24 +0200

    Fix: Reformattage du code

--- public/assets/js/Nav.js
+++ public/assets/js/Nav.js
@@ -5,25 +5,25 @@ const body = document.body;
 const linksText = document.querySelectorAll(".links .item a p");
 
 toggleBtn.addEventListener("click", async (e) => {
-	e.preventDefault();
-	if (!toggleBtn.className.includes("active")) {
-		toggleBtn.classList.add("active");
-		links.classList.add("active");
-		navbar.classList.add("active");
-	} else {
-		toggleBtn.classList.remove("active");
-		links.classList.remove("active");
-		navbar.classList.remove("active");
-	}
+  e.preventDefault();
+  if (!toggleBtn.className.includes("active")) {
+    toggleBtn.classList.add("active");
+    links.classList.add("active");
+    navbar.classList.add("active");
+  } else {
+    toggleBtn.classList.remove("active");
+    links.classList.remove("active");
+    navbar.classList.remove("active");
+  }
 });
 
-window.addEventListener("scroll", (e)=> {
-	if (body.offsetWidth <= 850) {
-		linksText.forEach(link=> link.style = "display: block;")
-	}
-	if (window.scrollY >= 50) {
-		navbar.classList.add("scrolled");
-	} else {
-		navbar.classList.remove("scrolled");
-	}
-})
+window.addEventListener("scroll", () => {
+  if (body.offsetWidth <= 850) {
+    linksText.forEach((link) => (link.style = "display: block;"));
+  }
+  if (window.scrollY >= 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
