e2a43d29c42f4c4fe4888573f1abe1240d36828e
Skycel9 <cece.burst@gmail.com>
2022-04-10 04:23:31 +0200

    Feat: Ajout de la section knowledges

--- public/assets/js/Knowledges.js
+++ public/assets/js/Knowledges.js
@@ -2,15 +2,17 @@ const toolsBtn = document.getElementById("tools-btn");
 const technosBtn = document.getElementById("technology-btn");
 const tools = document.getElementById("tools");
 const technos = document.getElementById("technology");
-const tabs = document.querySelector(".tabs")
+const table = document.querySelector(".table");
 
-toolsBtn.addEventListener('click', () => {
-    tools.classList.remove('hidden');
-    technos.classList.add('hidden');
-    tabs.classList.add("tools");
+console.log(table);
+
+toolsBtn.addEventListener("click", () => {
+  tools.classList.remove("hidden");
+  technos.classList.add("hidden");
+  table.classList.add("toggled");
 });
-technosBtn.addEventListener('click', () => {
-  technos.classList.remove('hidden');
-  tools.classList.add('hidden');
-  tabs.classList.remove("tools");
+technosBtn.addEventListener("click", () => {
+  technos.classList.remove("hidden");
+  tools.classList.add("hidden");
+  table.classList.remove("toggled");
 });
